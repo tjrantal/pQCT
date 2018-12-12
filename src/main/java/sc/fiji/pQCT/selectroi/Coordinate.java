3a8061aae435d54944f6a5254c0fbe48231b4d14
Timo Rantalainen <tjrantal@gmail.com>
2018-12-12 06:52:57 +0200

    Added Coordinate.java

---
+++ src/main/java/sc/fiji/pQCT/selectroi/Coordinate.java
@@ -0,0 +1,59 @@
+/*
+BSD 2-Clause License
+
+Copyright (c) 2018, Timo Rantalainen
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package sc.fiji.pQCT.selectroi;
+
+public class Coordinate{
+	public double ii;
+	public double jj;
+	/*Constructors*/
+	public Coordinate(){
+		this(-1,-1);
+	}
+	
+	public Coordinate(double ii,double jj){
+		this.ii = ii;
+		this.jj = jj;
+	}
+	
+	public Coordinate subtract(Coordinate a){
+		return new Coordinate(this.ii-a.ii,this.jj-a.jj);
+	}
+	
+
+	
+	public double maxVal(){
+		return max(Math.abs(ii),Math.abs(jj));
+	}
+	
+	private double max(double a,double b){
+		return a >= b ? a:b;
+	}
+	
+	
+}
